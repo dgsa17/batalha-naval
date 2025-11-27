d87206af7407a4bda10b67c9781bc1df1d521bf7
Renato060220 <renatolopesfernandes9@gmail.com>
2025-11-27 16:32:37 -0300

    Ajuste board.h

--- src/board.h
+++ src/board.h
@@ -1,23 +1,25 @@
 #ifndef BOARD_H
 #define BOARD_H
-#include <stdbool.h>
-#include "fleet.h"
 
+#include <stdbool.h>
+
+typedef struct Fleet Fleet;
 
 typedef enum {
     CELL_WATER,
     CELL_SHIP,
     CELL_HIT,
     CELL_MISS
-} CellState; 
+} CellState;
 
 typedef struct {
     CellState state;
-    int ship_id; 
+    int ship_id;
 } Cell;
+
 typedef struct {
     int rows, cols;
-    Cell *cells; 
+    Cell *cells;
 } Board;
 
 void initBoard(Board *b, int rows, int cols);
