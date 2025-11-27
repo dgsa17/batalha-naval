c90167d85884a86f2c9e4ba5b7380e1ffc90755c
Renato060220 <renatolopesfernandes9@gmail.com>
2025-11-27 16:42:11 -0300

    Volta fleet.h

--- src/fleet.h
+++ src/fleet.h
@@ -1,26 +1,23 @@
-#ifndef FLEET_H
-#define FLEET_H
+#ifndef FLEET_H 
+#define FLEET_H 
 
-#include <stdbool.h>
-
-struct Board;    
-typedef struct Board Board;
-
-typedef struct {
-    int size;
-    int hits;
-    bool sunk;
+#include <stdbool.h> 
+#include "board.h" 
+typedef struct { 
+int size; // tamanho do navio 
+int hits; // quantas partes foram atingidas 
+bool sunk; // true se afundou 
 } Ship;
 
-typedef struct {
-    Ship *ships;
-    int count;
-} Fleet;
+typedef struct { 
+Ship *ships; // vetor de navios 
+int count; // quantidade de navios 
+} Fleet; 
 
 void initFleet(Fleet *f);
 bool allShipsDestroyed(Fleet *f);
-bool canPlaceShip(Board *b, int r, int c, int size, bool horizontal);
-void placeShip(Board *b, Fleet *f, int ship_id, int r, int c, bool horizontal);
+bool canPlaceShip(Board *b, int r, int c, int size, bool horizontal); 
+void placeShip(Board *b, Fleet *f, int ship_id, int r, int c, bool horizontal); 
 void registerHit(Fleet *f, int ship_id);
 
 #endif
